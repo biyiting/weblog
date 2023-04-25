f3fd9e85d7e9201b12e330e8299f0bbd74cc9a36
粽子 <76830149+1105389168@users.noreply.github.com>
2023-04-25 09:13:25 +0800

    tag界面开发

--- themes/tcRicePudding/scripts/helper/tag_list.js
+++ themes/tcRicePudding/scripts/helper/tag_list.js
@@ -1,14 +1,29 @@
-const generatorDomString = (map) => {
+
+const generatorTagDomString = (path, name) => `<a class="tag" href=${path}>#${name}<a/>`;
+
+const generatorDomString = (map, helperFn = {}) => {
   let domStr = [];
+
   Object.entries(map).reverse().forEach(([year, postList]) => {
-    // todo
-    domStr.push(`<div>${year}</div>`);
-    postList.forEach(post => {
-      domStr.push(`<div>${post.title}</div>`);
+    domStr.push(`<div class="tag-year-title">${year}</div>`);
+    postList.forEach((post, inx) => {
+      domStr.push(
+        `<div class="tag-item">
+            <div class="tag-item-img"></div>
+            <div class="tag-item-content">
+              <div class="tag-item-content-title">${post.title}</div>
+              <div class="tag-item-content-list">${post.tags.map(tag => generatorTagDomString(helperFn.url_for(tag.path), tag.name)).join('')}</div>
+              <time class="tag-item-content-date" datetime=${post.date}>
+                <i class="iconfont icon-riqi2 iconfont-size"></i>
+                <span>${post.formatDate}</span>
+              </time>
+              <div class="tag-item-content-index">${inx + 1}</div>
+            </div>
+         </div>`);
     })
   });
 
-  return domStr.join('');
+  return `<div class="tag-list-helper">${domStr.join('')}</div>`;
 };
 
 // tag 主界面
@@ -18,10 +33,15 @@ hexo.extend.helper.register("tag_list", function (page = {}) {
     // 2022:[],
   };
 
-  page.posts.filter(it => it.formatDate = this.date(it.date, 'YYYY')).forEach(post => {
-    let key = post.formatDate;
+  page.posts.filter(it => {
+    it.formatYear = this.date(it.date, 'YYYY');
+    it.formatDate = this.date(it.date, 'YYYY-MM-DD');
+    it.formatDate = this.date(it.date, 'YYYY-MM-DD');
+    return it;
+  }).forEach(post => {
+    let key = post.formatYear;
     Array.isArray(map[key]) ? map[key].push(post) : map[key] = [post];
   });
 
-  return generatorDomString(map);
+  return generatorDomString(map, this);
 });
